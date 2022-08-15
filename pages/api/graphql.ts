e05d1c181d0842e54ab1b5e14c162aeb514ddb83
Arun Kumar <kumar05.962013@gmail.com>
2022-08-15 16:25:16 +0530

    creating types using nexus

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,13 +1,13 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { ApolloServer } from "apollo-server-micro";
-import { typeDefs } from "../../graphl/schema";
-import { resolvers } from "../../graphl/resolvers";
-import { createContext } from "../../graphl/context";
+import { schema } from "../../graphql/schema";
+import { resolvers } from "../../graphql/resolvers";
+import { createContext } from "../../graphql/context";
 import Cors from "micro-cors";
 
 const cors = Cors();
 const apolloServer = new ApolloServer({
-  typeDefs,
+  schema,
   resolvers,
   context: createContext,
 });
