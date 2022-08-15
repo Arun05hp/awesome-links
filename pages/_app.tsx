b37100b69fcff43ea8423581f4e3c4b33d052802
Arun Kumar <kumar05.962013@gmail.com>
2022-08-15 21:49:55 +0530

    auth0 authentication

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,14 +3,17 @@ import type { AppProps } from "next/app";
 import Layout from "../components/Layout";
 import { ApolloProvider } from "@apollo/client";
 import apolloClient from "../lib/apollo";
+import { UserProvider } from "@auth0/nextjs-auth0";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ApolloProvider client={apolloClient}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ApolloProvider>
+    <UserProvider>
+      <ApolloProvider client={apolloClient}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ApolloProvider>{" "}
+    </UserProvider>
   );
 }
 
