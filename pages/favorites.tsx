44af2b4b21d3c35928e06468daa7decc324d0d32
Arun Kumar <kumar05.962013@gmail.com>
2022-08-16 22:14:03 +0530

    favorites page

---
+++ pages/favorites.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Favorites = () => {
+  return (
+    <div className="mx-auto my-20 max-w-5xl px-10">
+      <h1 className="text-3xl font-medium my-5">My Favorites</h1>
+    </div>
+  );
+};
+
+export default Favorites;
